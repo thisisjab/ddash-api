9133b4267d47b6f3dc82a50d9416d2875e9e18d8
thisisjab <heythisisjab@gmail.com>
2024-09-27 20:50:57 +0330

    feat(projects): add project list route

--- api/projects/routes.py
+++ api/projects/routes.py
@@ -10,6 +10,14 @@ from api.users.auth.dependencies import AuthenticatedUser
 router = APIRouter(prefix="/projects", tags=["Projects"])
 
 
+@router.get("", response_model=list[ProjectOut], status_code=status.HTTP_200_OK)
+async def get_projects(
+    service: Annotated[ProjectService, Depends()],
+    authenticated_user: AuthenticatedUser,
+):
+    return await service.get_projects_of_user(user_id=authenticated_user.id)
+
+
 @router.post("", response_model=ProjectOut, status_code=status.HTTP_201_CREATED)
 async def create_project(
     data: ProjectIn,
--- api/projects/services.py
+++ api/projects/services.py
@@ -1,5 +1,7 @@
 from uuid import UUID
 
+from sqlalchemy import select
+
 from api.database.dependencies import AsyncSession
 from api.projects.models import Project
 from api.projects.schemas import ProjectIn, ProjectOut
@@ -8,6 +10,16 @@ from api.projects.schemas import ProjectIn, ProjectOut
 class ProjectService:
     def __init__(self, session: AsyncSession) -> None:
         self.async_session = session
+
+    async def get_projects_of_user(self, user_id: UUID) -> list[ProjectOut]:
+        async with self.async_session.begin() as session:
+            q = (
+                select(Project)
+                .where(Project.creator_id == user_id)
+                .order_by(Project.created_at.desc())
+            )
+            result = await session.execute(q)
+            return result.scalars()
 
     async def create_project(
         self, data: ProjectIn, creator_id: UUID | str
