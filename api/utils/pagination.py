4625d3d9e423aaf0f4467509b98f7b7c05c6f4ce
thisisjab <heythisisjab@gmail.com>
2024-11-08 18:22:37 +0330

    refactor(utils.pagination): improve logic

--- api/utils/pagination.py
+++ api/utils/pagination.py
@@ -1,16 +1,18 @@
+import math
 from typing import Annotated, Generic, Self, TypeVar
 
+from fastapi import HTTPException, status
 from fastapi import Query
 from pydantic import BaseModel, Field
-from sqlalchemy import ClauseElement
-
-from api.database.repository import RepositoryBase
+from sqlalchemy import func
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.sql.expression import Select
 
 T = TypeVar("T", bound=BaseModel)
 
 
 class PaginationParams(BaseModel):
-    per_page: int = Field(gt=1, lt=50, default=10)
+    per_page: int = Field(ge=1, le=50, default=10)
     page: int = Field(gt=0, default=1)
 
 
@@ -24,23 +26,28 @@ class PaginatedResponse(BaseModel, Generic[T]):
     items: list[T] = []
 
     async def paginate(
-        self,
-        repo: RepositoryBase,
-        per_page: int = 25,
-        page: int = 1,
-        filter_clauses: list[ClauseElement] = [],
-        order_clauses: list[ClauseElement] = [],
+            self,
+            query: Select,
+            session: async_sessionmaker,
+            pagination_params: PaginationParams,
     ) -> Self:
-        items_count = await repo.get_all_count()
-        self.total_pages = (items_count // per_page) + 1
-        self.current_page = page
-        limit = per_page
-        offset = (page - 1) * per_page
-        self.items = await repo.get_all(
-            limit=limit,
-            offset=offset,
-            filter_clauses=filter_clauses,
-            order_clauses=order_clauses,
-        )
-        self.count = len(self.items)
-        return self
+        async with session() as ac:
+            count_query = query.with_only_columns(func.count()).order_by(None)
+            count_result = await ac.execute(count_query)
+            items_count = count_result.scalar()
+
+            self.total_pages = math.ceil(items_count / pagination_params.per_page)
+
+            if pagination_params.page > self.total_pages:
+                raise HTTPException(detail="Invalid page.", status_code=status.HTTP_400_BAD_REQUEST)
+
+            self.current_page = pagination_params.page
+            limit = pagination_params.per_page
+            offset = (pagination_params.page - 1) * limit
+            query = query.limit(limit).offset(offset)
+            items_result = (await ac.execute(query)).scalars().all()
+            self.items = items_result
+            self.count = len(items_result)
+            return self
+
+#
