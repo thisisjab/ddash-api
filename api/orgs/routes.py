20e185517fe01c71fe4fd5834ef55ba926f0c12c
thisisjab <heythisisjab@gmail.com>
2024-11-08 18:33:19 +0330

    feat(orgs): add user's organizations collection endpoint

--- api/orgs/routes.py
+++ api/orgs/routes.py
@@ -1,21 +1,44 @@
 from typing import Annotated
+from uuid import UUID
 
-from fastapi import status, Depends
+from fastapi import Depends, HTTPException, Path, Query, status
 from fastapi.routing import APIRouter
 
 from api.orgs.schemas import OrganizationRequest, OrganizationResponse
 from api.orgs.services import OrganizationService
 from api.users.auth.dependencies import AuthenticatedUser
+from api.utils.pagination import PaginatedResponse, PaginationParams
 
-router = APIRouter(prefix="/organizations", tags=["organizations"])
+router = APIRouter(prefix="", tags=["organizations"])
+
+
+@router.get(
+    "/users/{user_id}/organizations",
+    response_model=PaginatedResponse[OrganizationResponse],
+)
+async def get_organizations(
+    user_id: Annotated[UUID, Path()],
+    user: AuthenticatedUser,
+    service: Annotated[OrganizationService, Depends()],
+    pagination_params: Annotated[PaginationParams, Query()],
+):
+    if user.id == user_id:
+        return await service.get_users_organizations(user, pagination_params)
+
+    raise HTTPException(
+        detail="Permissions for non-owner users have not been implemented, yet.",
+        status_code=status.HTTP_403_FORBIDDEN,
+    )
 
 
 @router.post(
-    "", response_model=OrganizationResponse, status_code=status.HTTP_201_CREATED
+    "/organizations",
+    response_model=OrganizationResponse,
+    status_code=status.HTTP_201_CREATED,
 )
 async def create_organization(
-        data: OrganizationRequest,
-        user: AuthenticatedUser,
-        service: Annotated[OrganizationService, Depends()],
+    data: OrganizationRequest,
+    user: AuthenticatedUser,
+    service: Annotated[OrganizationService, Depends()],
 ):
     return await service.create_organization_for_user(data=data, user=user)
--- api/orgs/services.py
+++ api/orgs/services.py
@@ -1,19 +1,29 @@
+from sqlalchemy import select
+
+from api.database.dependencies import AsyncSession
 from api.orgs.models import Organization
 from api.orgs.schemas import OrganizationRequest, OrganizationResponse
 from api.users.models import User
-from api.database.dependencies import AsyncSession
+from api.utils.pagination import PaginatedResponse, PaginationParams
 
 
 class OrganizationService:
     def __init__(self, session: AsyncSession):
         self.session = session
 
+    async def get_users_organizations(
+        self, user: User, pagination_params: PaginationParams
+    ) -> PaginatedResponse[OrganizationResponse]:
+        query = select(Organization).where(Organization.manager_id == user.id)
+        return await PaginatedResponse().paginate(
+            query, self.session, pagination_params
+        )
 
     async def create_organization_for_user(
         self, data: OrganizationRequest, user: User
     ) -> OrganizationResponse:
         async with self.session.begin() as ac:
-            instance =Organization(**data.model_dump(), manager_id=user.id)
+            instance = Organization(**data.model_dump(), manager_id=user.id)
             ac.add(instance)
             await ac.flush()
             await ac.refresh(instance)
