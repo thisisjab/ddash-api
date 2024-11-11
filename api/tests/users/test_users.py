547a79477d830c08b2296cce8fe0ecc40f25809a
thisisjab <heythisisjab@gmail.com>
2024-11-12 01:13:47 +0330

    test(users): test for duplicate users

--- api/tests/users/test_users.py
+++ api/tests/users/test_users.py
@@ -6,6 +6,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from api.users.models import User
 
 
+@pytest.fixture
+async def created_user(session: AsyncSession) -> User:
+    user = User(email="user@foo.buz", password="very_secret")
+    session.add(user)
+    await session.flush()
+    await session.refresh(user)
+    return user
+
+
 @pytest.mark.anyio
 async def test_user_create(ac: AsyncClient, session: AsyncSession):
     payload = {"email": "foo@bar.buz", "password": "foobar"}
@@ -18,3 +27,12 @@ async def test_user_create(ac: AsyncClient, session: AsyncSession):
     assert user is not None
     assert str(user.id) == response.json()["id"]
     assert response.status_code == 201
+
+
+@pytest.mark.anyio
+async def test_duplicate_user_cannot_be_created(
+    ac: AsyncClient, session: AsyncSession, created_user: User
+):
+    payload = {"email": created_user.email, "password": "very_secret"}
+    response = await ac.post("/users", json=payload)
+    assert response.status_code == 400
